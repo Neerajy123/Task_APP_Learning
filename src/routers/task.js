c3f18feb25f82978cdbcc5b1292616547adcd9a7
Neeraj <nee9034@gmail.com>
2023-11-06 15:23:12 +0530

    Sorting, Pagination, Filtering

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,11 +19,34 @@ router.post('/tasks', auth, async (req, res) => {
   }
 });
 
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=0
+// GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
+  const match = {};
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true';
+  }
+
+  const sort = {};
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':');
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+  }
+
   try {
     // const tasks = await Task.find({});
     // const tasks = await Task.find({ owner: req.user._id });
-    await req.user.populate('tasks');
+    await req.user.populate({
+      path: 'tasks',
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort,
+      },
+    });
     await res.send(req.user.tasks);
   } catch (e) {
     res.status(500).send();
